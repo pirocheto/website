1035aa8ccd56a58f8b997a7ed57f89df89ffa05d
pirocheto <pirocheto@gmail.com>
2024-12-08 11:09:41 +0100

    add github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://pirocheto.github.io",
   integrations: [
     starlight({
       title: "Pierre Rochet",
