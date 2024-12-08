4eb34e01c363ad055ba72e6d5e8e5226f5737408
pirocheto <pirocheto@gmail.com>
2024-12-08 11:20:52 +0100

    fix: base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind"
 // https://astro.build/config
 export default defineConfig({
   site: "https://pirocheto.github.io",
+  base: "/website/",
   integrations: [
     starlight({
       title: "Pierre Rochet",
